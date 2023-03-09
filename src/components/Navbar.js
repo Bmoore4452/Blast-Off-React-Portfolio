6059e4f7a55ae0d4895a6aea3b61523fd26fe796
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-03-09 09:18:35 -0500

    setup gh-pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 export default function Navbar({ setPage }) {
   return (
-    <nav class="nav d-flex justify-content-end">
+    <nav class="nav d-flex justify-content-end mainNav ">
       <h1 className="name">Brian Moore</h1>
       <a class="nav-link" href="#about" onClick={() => setPage('about')}>
         About Me
       </a>
-      <a class="nav-link" href="#portfolio" onClick={() => setPage('portfolio')}>
+      <a
+        class="nav-link"
+        href="#portfolio"
+        onClick={() => setPage('portfolio')}
+      >
         Portfolio
       </a>
       <a class="nav-link" href="#contact" onClick={() => setPage('contact')}>
